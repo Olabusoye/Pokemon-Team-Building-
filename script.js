009a147694e6fc132bed1eb0904ba519936aef9c
George Reeve <george.fr.reeve@gmail.com>
2022-10-27 10:10:19 +0100

    Add spacing and return.

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 async function getData(input) {
     const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${input}`);
     const data = await response.json();
+
     let types = [];
     data.types.forEach(type => types.push(upperFirstChar(type.type.name)));
-    console.log({
+    return {
         name: upperFirstChar(data.name),
         image: data.sprites.front_default,
         types
-    })
+    }
 }
 
 function upperFirstChar(str) {
