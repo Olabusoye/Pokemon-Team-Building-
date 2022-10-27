c8a2e139b9f505f1250f0c089a3dcb5e48a32fd1
George Reeve <george.fr.reeve@gmail.com>
2022-10-27 15:36:46 +0100

    Add element functionality.

--- script.js
+++ script.js
@@ -47,3 +47,42 @@ async function getData(input) {
 function upperFirstChar(input) {
     return input.charAt(0).toUpperCase() + input.slice(1);
 }
+
+const form = document.getElementById("search-form");
+form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const formData = new FormData(form);
+    const search = formData.get("search");
+
+    const data = await getData(search);
+    console.log(data);
+
+    const cards = document.getElementById("slots");
+    const card = [...cards.children].find(card => card.classList.contains("empty"));
+    card.innerHTML = "";
+
+    const name = document.createElement("h2");
+    name.classList.add("name");
+    name.innerText = data.name;
+
+    const imageContainer = document.createElement("div");
+    imageContainer.classList.add("image");
+
+    const image = document.createElement("img");
+    image.src = data.image;
+    imageContainer.append(image);
+
+    const infoContainer = document.createElement("info");
+    infoContainer.classList.add("info");
+    const typeElements = [];
+    data.types.forEach(type => {
+        const span = document.createElement("span");
+        span.classList.add(colours[type]);
+        span.innerText = type;
+        typeElements.push(span);
+    });
+    typeElements.forEach(typeElement => infoContainer.append(typeElement));
+
+    card.append(name, imageContainer, infoContainer);
+    card.classList.remove("empty");
+});
